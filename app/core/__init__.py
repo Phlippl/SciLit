bf5f76870d530a0dd73c4ea0395cff68fb4bee91
phlippl <philipp.wei@gmail.com>
2025-03-29 13:52:20 +0100

    Anpassung Core Module

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,9 +1,12 @@
 """
 Core-Module für SciLit
 """
+from .document.processor import DocumentProcessor
+from .document.parsers import DocumentParser, DocumentParsingError
+from .metadata.formatter import MetadataFormatter  
+from .analysis.text_splitter import TextSplitter  
+from app.utils.file_utils import generate_document_id  
+from .metadata.formatter import format_citation  
+from app.api import MetadataAPIClient
 
-from .document_processor import DocumentProcessor
-from .document_parsers import DocumentParser, DocumentParsingError
-from .metadata_api_client import MetadataAPIClient
-from .text_analysis import TextAnalyzer
-from .utils import generate_document_id, format_citation
+
